d83bb32fecc1f0b17f210c655967c9b98d5f53de
luochunhai <luo.chunhai@navercorp.com>
2021-01-18 16:13:48 +0800

    npm install bootstrap@3 --save-dev ; index.css -> @import url('../node_modules/bootstrap/dist/css/bootstrap.css');

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import SearchApp from "./component/search/SearchApp";
 import CommentApp from "./component/comment/app/CommentApp";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import RouterApp from "./component/router/SearchApp";
+import Home from "./component/Home";
 
 class App extends React.Component {
     render() {
@@ -9,27 +11,19 @@ class App extends React.Component {
             <Router>
                 <div>
                     <nav>
-                        <ul>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/search">Search</Link>
-                            </li>
-                            <li>
-                                <Link to="/comment">Comment</Link>
-                            </li>
+                        <ul className="nav nav-pills">
+                            <li role="presentation" className="active"><Link to="/">Home</Link></li>
+                            <li role="presentation"><Link to="/search">Search</Link></li>
+                            <li role="presentation"><Link to="/comment">Comment</Link></li>
+                            <li role="presentation"><Link to="/router">React Router</Link></li>
                         </ul>
                     </nav>
+
                     <Switch>
-                        <Route path="/search">
-                            <SearchApp />
-                        </Route>
-                        <Route path="/comment">
-                            <CommentApp />
-                        </Route>
-                        <Route path="/">
-                        </Route>
+                        <Route path="/search"><SearchApp/></Route>
+                        <Route path="/comment"><CommentApp/></Route>
+                        <Route path="/router"><RouterApp/></Route>
+                        <Route path="/"><Home/></Route>
                     </Switch>
                 </div>
             </Router>
@@ -37,4 +31,5 @@ class App extends React.Component {
     }
 
 }
+
 export default App;
---
+++ react_app/src/component/router/SearchApp.jsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+class RouterApp extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                router
+            </div>
+        )
+    }
+}
+
+export default RouterApp;
