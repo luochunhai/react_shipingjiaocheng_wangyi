68c862b420c64edc27f32ce93cb90718d7d1e39b
luochunhai <luo.chunhai@navercorp.com>
2021-01-20 13:54:02 +0800

    43 redux chrom 调试

--- react_app/src/component/react-redux/redux/store.js
+++ react_app/src/component/react-redux/redux/store.js
@@ -2,11 +2,15 @@ import {applyMiddleware, createStore} from 'redux'
 import {counter} from './reducers'
 
 import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 //生成一个store 对象
 //内部会第一次调用reducer,得到初始state
 //applyMiddleware(thunk)  应用异步中间件
-const store = createStore(counter, applyMiddleware(thunk))
+const store = createStore(
+    counter,
+    composeWithDevTools(applyMiddleware(thunk))
+)
 console.log(store, store.getState())
 
 export default store
